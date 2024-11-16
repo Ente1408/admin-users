148b3b173d7aaf7035cb9326f81ca1d5711c9cac
samuel.obando <samuel.obando@queen.com.co>
2024-11-16 17:25:23 -0500

    Modelos de cada una de las tablas correspondientes para el aplicativo

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -18,9 +21,17 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'document_type_id',
+        'document_number',
+        'city_id',
+        'address',
+        'phone_code_id',
+        'phone',
         'email',
         'password',
+        'status',
     ];
 
     /**
@@ -42,4 +53,44 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Get the user's image.
+     */
+    public function image(): MorphOne
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
+
+    /**
+     * Get the city that owns the user.
+     */
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    /**
+     * Get the document_type that owns the user.
+     */
+    public function document_type(): BelongsTo
+    {
+        return $this->belongsTo(DocumentType::class);
+    }
+
+    /**
+     * Get the phone_code that owns the user.
+     */
+    public function phone_code(): BelongsTo
+    {
+        return $this->belongsTo(Country::class, 'phone_code_id');
+    }
+
+    /**
+     * The roles that belong to the user.
+     */
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class, 'role_user');
+    }
 }
